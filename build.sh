8e35a5fbb15d3e3e4af8d2731315cff8d0dcc1c7
Clara Attermo <clara.attermo@gmail.com>
2017-10-08 17:29:11 +0200

    trying build script

--- build.sh
+++ build.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 cd out
+echo $FTP_USER
+echo ${FTP_USER}
 find * -type f -exec "curl -u $FTP_USER:$FTP_PASSWORD --ftp-create-dirs -T {} ftp://188.95.227.17/fyrton.se/public_html/{} " \;

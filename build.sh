edc83f9903e2d677d5e6de89d204b9b6ae5546d4
Clara Attermo <clara.attermo@gmail.com>
2017-10-08 17:45:40 +0200

    trying build script

--- build.sh
+++ build.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-npm run test
+npm run generate
 cd out
-echo $FTP_USER
-echo ${FTP_USER}
+ls
 find * -type f -exec "curl -u $FTP_USER:$FTP_PASSWORD --ftp-create-dirs -T {} ftp://188.95.227.17/fyrton.se/public_html/{} " \;

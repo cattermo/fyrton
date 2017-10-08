472a3b205aa0278ba4838832d53749157810e63c
Clara Attermo <clara.attermo@gmail.com>
2017-10-08 17:56:41 +0200

    trying build script

--- build.sh
+++ build.sh
@@ -2,4 +2,4 @@
 npm run generate
 cd out
 ls
-find * -type f -exec "curl -u $FTP_USER:$FTP_PASSWORD --ftp-create-dirs -T {} ftp://188.95.227.17/fyrton.se/public_html/{}" \;
+find * -type f -exec curl -u $FTP_USER:$FTP_PASSWORD --ftp-create-dirs -T {} ftp://188.95.227.17/fyrton.se/public_html/{} \;

ccde2f1b189c57cce087e7a8e726a489fa293a1a
cattermo <clara.attermo@gmail.com>
2018-09-28 23:17:13 +0200

    ftp url

--- build.sh
+++ build.sh
@@ -2,4 +2,4 @@
 npm run generate
 cd out
 ls
-find * -type f -exec curl -u $FTP_USER:$FTP_PASSWORD --ftp-create-dirs -T {} ftp://188.95.227.17/fyrton.se/public_html/{} \;
+find * -type f -exec curl -u $FTP_USER:$FTP_PASSWORD --ftp-create-dirs -T {} ftp://ftpcluster.loopia.se/fyrton.se/public_html/{} \;

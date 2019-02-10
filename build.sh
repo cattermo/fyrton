9c724b4f632930c0c1ffd1c83aa21b24080215f1
cattermo <clara.attermo@gmail.com>
2019-02-10 17:07:11 +0100

    remove travis, circleci used instead

--- build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-npm run generate
-cd out
-ls
-find * -type f -exec curl --verbose --progress-bar --ipv4 --connect-timeout 8 \
-     --max-time 120 --retry 128 --ftp-ssl --disable-epsv --ftp-pasv \
-     -u $FTP_USER:$FTP_PASSWORD --ftp-create-dirs -T {} ftp://ftpcluster.loopia.se/{} \;

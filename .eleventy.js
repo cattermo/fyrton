833d53c5e4de48ca84fd7d39635f39851d83a3b0
cattermo <clara.attermo@gmail.com>
2025-09-30 13:01:20 +0200

    Sitemap, robots.txt

--- .eleventy.js
+++ .eleventy.js
@@ -1,43 +1,52 @@
 const generateThumbnail = require("./utils/image");
 const fs = require("fs");
 const path = require("path");
+const sitemap = require("@quasibit/eleventy-plugin-sitemap");
 
 module.exports = function (eleventyConfig) {
-  const isProd = process.env.NODE_ENV === 'production';
+    const isProd = process.env.NODE_ENV === 'production';
 
-  eleventyConfig.addFilter("viteAsset", function (originalPath) {
-    if (!isProd) {
-      return `/${originalPath}`; // e.g. "/src/styles/main.less"
-    }
-    const manifestPath = path.resolve(__dirname, "dist/.vite/manifest.json");
-    if (!fs.existsSync(manifestPath)) return originalPath;
-    const manifest = JSON.parse(fs.readFileSync(manifestPath));
-    const hashed = manifest[originalPath];
-    return hashed ? `/${hashed.file}` : originalPath;
-  });
+    eleventyConfig.addPlugin(sitemap, {
+        lastModifiedProperty: "modified",
+        sitemap: {
+            hostname: "https://fyrton.se",
+        },
+    });
 
-  eleventyConfig.addFilter("keysDesc", function (obj) {
-    return Object.keys(obj).sort((a, b) => b - a);
-  });
+    eleventyConfig.addFilter("viteAsset", function (originalPath) {
+        if (!isProd) {
+            return `/${originalPath}`; // e.g. "/src/styles/main.less"
+        }
+        const manifestPath = path.resolve(__dirname, "dist/.vite/manifest.json");
+        if (!fs.existsSync(manifestPath)) return originalPath;
+        const manifest = JSON.parse(fs.readFileSync(manifestPath));
+        const hashed = manifest[originalPath];
+        return hashed ? `/${hashed.file}` : originalPath;
+    });
 
-  eleventyConfig.addNunjucksAsyncShortcode("thumbnail", async function (src, alt, className) {
-    return await generateThumbnail(src, alt, className);
-  });
+    eleventyConfig.addFilter("keysDesc", function (obj) {
+        return Object.keys(obj).sort((a, b) => b - a);
+    });
 
-  // Copy non-bundled assets
-  eleventyConfig.addPassthroughCopy({ "src/images": "images" });
-  eleventyConfig.addPassthroughCopy("src/files");
-  eleventyConfig.addPassthroughCopy({"dist/assets": "assets"});
+    eleventyConfig.addNunjucksAsyncShortcode("thumbnail", async function (src, alt, className) {
+        return await generateThumbnail(src, alt, className);
+    });
 
-  return {
-    dir: {
-      input: "src",
-      includes: "_includes",
-      data: "_data",
-      output: "_site"
-    },
-    markdownTemplateEngine: "njk",
-    htmlTemplateEngine: "njk",
-    templateFormats: ["njk", "md", "html"]
-  };
+    // Copy non-bundled assets
+    eleventyConfig.addPassthroughCopy({"src/images": "images"});
+    eleventyConfig.addPassthroughCopy("src/files");
+    eleventyConfig.addPassthroughCopy({"dist/assets": "assets"});
+    eleventyConfig.addPassthroughCopy({"src/robots.txt": "/robots.txt"});
+
+    return {
+        dir: {
+            input: "src",
+            includes: "_includes",
+            data: "_data",
+            output: "_site"
+        },
+        markdownTemplateEngine: "njk",
+        htmlTemplateEngine: "njk",
+        templateFormats: ["njk", "md", "html"]
+    };
 };
